242579fbcd7122a7f82971265e94ddb51e904c6d
NganTruong2406 <ngantruong24062005@gmail.com>
2026-04-15 14:28:26 +0700

    Delete

--- src/main/java/com/ckfm/repository/FacilityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ckfm.repository;
-
-import com.ckfm.entity.Facility;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacilityRepository extends JpaRepository<Facility, Long> {
-}
